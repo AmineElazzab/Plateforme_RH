b5d9486d52e1ace7d12b27988f66a71ceee16e1d
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-25 14:58:39 +0100

    delete old folder

--- api/src/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express();
-const { Login, Register } = require("../controllers/userController");
-
-router.post("/login", Login).post("/register", Register);
-
-module.exports = router;
