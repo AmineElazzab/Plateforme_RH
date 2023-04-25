3404d0ba075c7d07d56760633d255e3499aa8313
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-25 15:01:27 +0100

    Models

---
+++ harmony-api/app/Models/Task.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Task extends Model {
+    static get primaryKey() {
+        return 'id'
+    }
+
+    static get createdAtColumn() {
+        return null
+    }
+
+    static get updatedAtColumn() {
+        return null
+    }
+
+    user() {
+        return this.belongsTo('App/Models/User')
+    }
+}
+
+module.exports = Task
