32d8bcf6a11887d51f190986544315ad31403c49
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-05-02 09:18:41 +0100

    Task migration

---
+++ harmony-api/database/migrations/1682434818359_task_schema.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TaskSchema extends Schema {
+  up() {
+    this.create('tasks', (table) => {
+      table.increments()
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.string('taskName ', 255).notNullable()
+      table.string('taskDescription', 255).notNullable()
+      table.string('feature', 255).notNullable()
+      table.string('taskType', 255).notNullable()
+      table.date('startDate').notNullable()
+      table.date('endDate').notNullable()
+      table.integer('proirity').notNullable()
+      table.integer('criticity').notNullable()
+      table.integer('estimatedWorkload ').notNullable()
+      table.string('assigned_to').notNullable()
+      table.enu('current_status', ['pending', 'in_progress', 'completed']).defaultTo('pending')
+      table.string('taskTree').notNullable()
+      
+
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('tasks')
+  }
+}
+
+module.exports = TaskSchema
