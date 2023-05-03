8ab38120c32116b1991eb736c2bce774f04bedee
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-05-03 11:45:53 +0100

    Update 1782516154092_task_schema.js
    Update task schema

--- harmony-api/database/migrations/1782516154092_task_schema.js
+++ harmony-api/database/migrations/1782516154092_task_schema.js
@@ -7,28 +7,23 @@ class TaskSchema extends Schema {
   up() {
     this.create("tasks", (table) => {
       table.increments("task_id");
-      table.string("task_title", 80).notNullable();
+      table.integer("task_project_id").unsigned().notNullable().references("project_id").inTable("projects");
+      table.string("task_name", 80).notNullable();
       table.string("task_description", 254).notNullable();
-      table.string("feature", 254).notNullable();
-      table.enu("complexity", ["low", "medium", "high"]).notNullable();
-      table
-        .enu("priority", ["low", "medium", "high", "critical"])
-        .notNullable();
-      table
-        .enu("dev_status", [
-          "to do",
-          "wip",
-          "pending",
-          "blocked",
-          "ready for test",
-          "rework",
-          "done",
-        ])
-        .notNullable();
-      table.integer("task_progress").notNullable().defaultTo(0);
+      table.string("task_feature", 254).notNullable();
+      table.string("task_type", 245).notNullable();
       table.date("task_start_date").notNullable();
       table.date("task_end_date").notNullable();
-      table.integer("task_estimated_workload").notNullable();
+      table.enu("task_priority", ["low", "medium", "high", "critical"]).notNullable();
+      table.enu("task_complexity", ["low", "medium", "high"]).notNullable();
+      table.enu("task_dev_status", ["to do","wip","pending","blocked","ready for test","rework","done",])
+        .notNullable().defaultTo("to do");
+      table.boolean("task_rework").notNullable().defaultTo(0);
+      table.boolean("task_functional_test_status").notNullable().defaultTo(0);
+      table.integer("task_progress").notNullable().defaultTo(0);
+      table.string("task_comments", 245).nullable();
+      table.integer("task_estimated_workload").nullable();
+      table.string("task_issue_status").nullable();
 
       table.timestamps();
     });
