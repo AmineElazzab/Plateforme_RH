8245dec39d2ff1a149b03a11e313b2fb4ae1e598
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-05-04 12:24:03 +0100

    Validator Create Risk

---
+++ harmony-api/app/Validators/CreateRisk.js
@@ -0,0 +1,45 @@
+"use strict";
+
+class CreateRisk {
+  get rules() {
+    return {
+      // validation rules
+      risk_name: "required",
+      risk_description: "required",
+      impact_description: "required",
+      impact_level: "required",
+      probability_level: "required",
+      criticality_level: "required",
+      mitigation_plan: "required",
+    };
+  }
+
+  get messages() {
+    return {
+      "risk_name.required": "You must provide a risk name.",
+      "risk_description.required": "You must provide a risk description.",
+      "impact_description.required": "You must provide a impact description.",
+      "impact_level.required": "You must provide a impact level.",
+      "probability_level.required": "You must provide a probability level.",
+      "criticality_level.required": "You must provide a criticality level.",
+      "mitigation_plan.required": "You must provide a mitigation plan.",
+    };
+  }
+
+  async fails(errorMessages) {
+    return this.ctx.response.status(422).json({
+      status: "error",
+      message: errorMessages[0].message,
+    });
+  }
+
+  get validateAll() {
+    return true;
+  }
+
+  async authorize() {
+    return true;
+  }
+}
+
+module.exports = CreateRisk;
