2fe15898912a5c772095251068ce1935a3f6d5f2
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-05-05 12:40:32 +0100

    change skill_user_id to user_id

--- harmony-api/database/migrations/1683190018965_skill_schema.js
+++ harmony-api/database/migrations/1683190018965_skill_schema.js
@@ -7,6 +7,11 @@ class SkillSchema extends Schema {
   up() {
     this.create("skills", (table) => {
       table.increments("skill_id");
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("user_id")
+        .inTable("users");
       table.string("skill_name", 255).notNullable();
       table.string("skill_description", 255).notNullable();
       table
