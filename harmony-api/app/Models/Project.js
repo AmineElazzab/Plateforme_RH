ebec4ce2abbe7488264428666335d25fa1f5c661
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-05-08 11:59:58 +0100

    user workload is -30 when project is deleted

--- harmony-api/app/Models/Project.js
+++ harmony-api/app/Models/Project.js
@@ -2,11 +2,37 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
+const ProjectCollaborator = use("App/Models/ProjectCollaborator");
 
 class Project extends Model {
   static get primaryKey() {
     return "project_id";
   }
+
+  static boot() {
+    super.boot();
+
+    this.addHook("beforeDelete", async (project) => {
+      try {
+        const collaborators = await ProjectCollaborator.query()
+          .where("project_id", project.project_id)
+          .fetch();
+
+        await Promise.all(
+          collaborators.rows.map(async (collaborator) => {
+            const user = await collaborator.users().fetch();
+            if (user) {
+              user.user_workload -= 30;
+              await user.save();
+            }
+          })
+        );
+      } catch (error) {
+        console.error("Error adjusting user workload:", error);
+      }
+    });
+  }
+
   departments() {
     return this.belongsTo("App/Models/Department");
   }
