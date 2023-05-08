ea02a164d98f65539f4a5414ad05449ea7786f1c
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-05-08 16:09:07 +0100

    test

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,19 +1,17 @@
-import { AxiosError } from 'axios';
-import client from '../lib/client';
-
-
+import { AxiosError } from "axios";
+import client from "../lib/client";
 
 export const login = async (body) => {
   try {
-    const { data } = await client.post('/auth/login', body);
+    const { data } = await client.post("/auth/login", body);
     return { data, error: null };
   } catch (err) {
     let errorMessage;
     if (err instanceof AxiosError) {
       // axios error
-      errorMessage = err.response?.data?.message || 'Something Went Wrong';
+      errorMessage = err.response?.data?.error || "Something Went Wrong";
     } else {
-      errorMessage = 'Something Went Wrong';
+      errorMessage = "Something Went Wrong";
     }
     return { data: null, error: errorMessage };
   }
