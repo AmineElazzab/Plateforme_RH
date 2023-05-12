0c0352b32e358e525252553cd4ca39eb6d6bf04d
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-05-12 08:38:24 +0100

    add Projects route and Utilisateurs route

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import Collaborator from "./pages/collaborator/Collaborator";
 import ProjectLeader from "./pages/project-leader/projectLeader";
 import ProjectManager from "./pages/project-manager/projectManager";
 import Admin from "./pages/administrator/Admin";
-import Utilisateurs from "./pages/administrator/Utilisateurs"
+import Projects from "./pages/administrator/Projects";
+import Utilisateurs from "./pages/administrator/Utilisateurs";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" exact element={<Utilisateurs />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/utilisateurs" element={<Utilisateurs />} />
 				<Route path="/collaborator" element={<Collaborator />} />
 				<Route path="/project-leader" element={<ProjectLeader />} />
 				<Route path="/project-manager" element={<ProjectManager />} />
