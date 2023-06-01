66cd4a645e5e103d2c2d69b20cfe1b4aed476402
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-01 14:15:23 +0100

    Update client.js

--- client/src/lib/client.js
+++ client/src/lib/client.js
@@ -1,27 +1,63 @@
-import axios from 'axios';
-import JWTToken from './token';
-import { API_URL } from './globals';
+import axios from "axios";
+import JWTToken from "./token";
+import { API_URL } from "./globals";
 
 const client = axios.create({
-	baseURL: API_URL,
+  baseURL: API_URL,
 });
 
+const refreshTokenClient = axios.create({
+  baseURL: API_URL,
+});
 
+refreshTokenClient.interceptors.request.use(
+  function (config) {
+    const refreshToken = JWTToken.getRefreshToken();
+
+    if (refreshToken) {
+      config.headers["Authorization"] = refreshToken;
+    }
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 client.interceptors.request.use(
-	function (config) {
-		// Do something before request is sent
-		const token = JWTToken.getToken();
+  async function (config) {
+    const token = JWTToken.getToken();
+    const refreshToken = JWTToken.getRefreshToken();
 
-		if (token) {
-			config.headers['Authorization'] = token;
-		}
+    if (token) {
+      const isTokenExpired = JWTToken.isTokenExpired(token);
 
-		return config;
-	},
-	function (error) {
-		return Promise.reject(error);
-	},
+      if (isTokenExpired) {
+        console.log("Token expired, refreshing...");
+        try {
+          const response = await refreshTokenClient.post("/auth/refresh-token", {
+            refresh_token: refreshToken,
+          });
+          const newToken = response.data.token;
+          JWTToken.setToken(newToken);
+          JWTToken.setRefreshToken(response.data.refreshToken);
+          config.headers["Authorization"] = newToken;
+          console.log("Token refreshed");
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        console.log("Token not expired, using existing token");
+        config.headers["Authorization"] = token;
+      }
+    }
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 
 export default client;
