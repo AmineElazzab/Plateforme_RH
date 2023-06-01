b886aa93716e3f49f62355b3ece6cfa884f6f6fe
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-01 14:15:41 +0100

    Update account-popover.js

--- client/src/layouts/dashboard/account-popover.js
+++ client/src/layouts/dashboard/account-popover.js
@@ -3,17 +3,24 @@ import { useRouter } from "next/navigation";
 import PropTypes from "prop-types";
 import { Box, Divider, MenuItem, MenuList, Popover, Typography } from "@mui/material";
 import { useAuth } from "src/hooks/use-auth";
+import { signOut } from "next-auth/react";
 
 export const AccountPopover = (props) => {
   const { anchorEl, onClose, open } = props;
   const router = useRouter();
   const auth = useAuth();
 
+  // const handleSignOut = useCallback(() => {
+  //   onClose?.();
+  //   auth.signOut();
+  //   router.push("/auth/login");
+  // }, [onClose, auth, router]);
+
   const handleSignOut = useCallback(() => {
     onClose?.();
-    auth.signOut();
-    router.push("/auth/login");
-  }, [onClose, auth, router]);
+    signOut({ callbackUrl: "/authentication/login", redirect: true });
+    router.push("/authentication/login");
+  }, [onClose, router]);
 
   return (
     <Popover
