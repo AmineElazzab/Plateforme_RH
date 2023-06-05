e202bd285c7d4d32143a15ecdc417cded790a628
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-05 15:31:31 +0100

    Update api_loaders.ts

--- client/src/loaders/api_loaders.ts
+++ client/src/loaders/api_loaders.ts
@@ -1,45 +1,43 @@
 import HttpClient, { KeyValue } from "../stores/http_store";
-import { IMyProfile, IOtherDetails, vanillaAccountStore } from '../stores/project_store';
+import { IProjectStore } from '../stores/project_store';
 
 
-export async function get_me(): Promise<IMyProfile>{
-  const url = `${HttpClient.base_url}/me`;
+export async function get_project(id : number): Promise<IProjectStore> {
+  const url = `${HttpClient.base_url}/project/${id}/`;
   return HttpClient.get(url).then((response) => {
     if (response && response.status === 200) {
-    	const me: IMyProfile = response.data
+    	const project: IProjectStore = response.data
 			console.log(url)
-			console.log(me)
-      vanillaAccountStore.setState({my_profile: {...me}})
-      console.log('set', vanillaAccountStore.getState().my_profile)
-    	return Promise.resolve(me);
+			console.log(project)
+      return Promise.resolve(project);
     }
     return Promise.reject(null);
   });
 }
 
 
-export async function add_friend(id: number){
-  const url = `${HttpClient.base_url}/add_friend/`;
-  return HttpClient.post({url, data: {id}}).then((response) => {
-    if (response && response.status === 200) {
-    	const them: IOtherDetails = response.data
-			console.log(url)
-			console.log(them)
-    	return Promise.resolve(them);
-    }
-    return Promise.reject(null);
-  });
+export async function add_project(){
+  
 }
 
-export async function remove_friend(id: number){
-  const url = `${HttpClient.base_url}/remove_friend/`;
-  return HttpClient.post({url, data: {id}}).then((response) => {
+export async function update_project(){
+  
+}
+
+export async function delete_project(){
+ 
+}
+
+export async function get_users(){
+  const url = `${HttpClient.base_url}/users`;
+  return HttpClient.get(url).then((response) => {
     if (response && response.status === 200) {
-    	const them: IOtherDetails = response.data
-			console.log(url)
-			console.log(them)
-    	return Promise.resolve(them);
+      const users = response.data;
+      // console.log(url)
+      // console.log(users)
+      return Promise.resolve(users);
     }
     return Promise.reject(null);
-  });
+  }
+  );
 }
