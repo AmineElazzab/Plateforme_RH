4546e8d6517c0b2c394ffa9d099027a901738444
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-08 12:34:55 +0100

    Update api_loaders.ts

--- client/src/loaders/api_loaders.ts
+++ client/src/loaders/api_loaders.ts
@@ -1,35 +1,27 @@
-import HttpClient, { KeyValue } from "../stores/http_store";
-import { IProjectStore } from '../stores/project_store';
+import HttpClient, { KeyValue } from '../stores/http_store';
+import { IProject } from '../interfaces/project';
 
-
-export async function get_project(id : number): Promise<IProjectStore> {
-  const url = `${HttpClient.base_url}/project/${id}/`;
+export async function get_project(id: number): Promise<IProject> {
+  const url = `${HttpClient.base_url}project/${id}/`;
   return HttpClient.get(url).then((response) => {
     if (response && response.status === 200) {
-    	const project: IProjectStore = response.data
-			console.log(url)
-			console.log(project)
+      const project: IProject = response.data;
+      console.log(url);
+      console.log(project);
       return Promise.resolve(project);
     }
     return Promise.reject(null);
   });
 }
 
+export async function add_project() {}
 
-export async function add_project(){
-  
-}
+export async function update_project() {}
 
-export async function update_project(){
-  
-}
+export async function delete_project() {}
 
-export async function delete_project(){
- 
-}
-
-export async function get_users(){
-  const url = `${HttpClient.base_url}/users`;
+export async function get_users() {
+  const url = `${HttpClient.base_url}users`;
   return HttpClient.get(url).then((response) => {
     if (response && response.status === 200) {
       const users = response.data;
@@ -38,6 +30,5 @@ export async function get_users(){
       return Promise.resolve(users);
     }
     return Promise.reject(null);
-  }
-  );
+  });
 }
