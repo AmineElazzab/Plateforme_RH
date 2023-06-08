60c9af045df039656592ade6d988e8faa9e41dd6
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-08 12:35:07 +0100

    Update useCurrentUser.ts

--- client/src/hooks/query-hooks/useCurrentUser.ts
+++ client/src/hooks/query-hooks/useCurrentUser.ts
@@ -1,24 +1,38 @@
 import { useQuery } from '@tanstack/react-query';
-import client from '~lib/client';
+// import client from '~lib/client';
+// import JWTToken from '~lib/token';
 import { COMMON_STALE_TIME } from '~lib/globals';
-import JWTToken from '~lib/token';
+import HttpClient from '../../stores/http_store';
 
-export function getCurrentsUsers(ctx: any = null) {
-  return async () => {
-    const token = JWTToken.getToken(ctx);
-    try {
-      const { data } = await client.get('/users/me', {
-        headers: {
-          Authorization: token,
-        },
-      });
-      return data;
-    } catch (error) {
-      JWTToken.removeToken();
-      return null;
+export async function getCurrentsUsers(ctx?: any) {
+  const url = `${HttpClient.base_url}users/me`;
+  return HttpClient.get(url).then((response) => {
+    if (response && response.status === 200) {
+      const user = response.data;
+      // console.log(url)
+      // console.log(user)
+      return Promise.resolve(user);
     }
-  };
+    return Promise.reject(null);
+  });
 }
+
+// export function getCurrentsUsers(ctx: any = null) {
+//   return async () => {
+//     const token = JWTToken.getToken(ctx);
+//     try {
+//       const { data } = await client.get('/users/me', {
+//         headers: {
+//           Authorization: token,
+//         },
+//       });
+//       return data;
+//     } catch (error) {
+//       JWTToken.removeToken();
+//       return null;
+//     }
+//   };
+// }
 
 export function useCurrentUser(ctx?: any) {
   // TODO: 🚩 check all critical arguments
@@ -30,9 +44,14 @@ export function useCurrentUser(ctx?: any) {
     filters = ctx.filters ?? {};
   }
 
-  return useQuery({
-    queryKey: ['current-user'],
-    queryFn: getCurrentsUsers(ctx),
+  //   return useQuery({
+  //     queryKey: ['current-user'],
+  //     queryFn: getCurrentsUsers(ctx),
+  //     staleTime: COMMON_STALE_TIME,
+  //   });
+  // }
+
+  return useQuery(['current-user'], () => getCurrentsUsers(ctx), {
     staleTime: COMMON_STALE_TIME,
   });
 }
