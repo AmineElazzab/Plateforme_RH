ff669ad2305262397a7c974f35176c1247b431dc
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-08 12:44:38 +0100

    Update index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,30 +1,22 @@
-
-import {  HiOfficeBuilding } from "react-icons/hi";
-import {BiDesktop} from "react-icons/bi";
-import type { NextPage } from 'next';
-
-import NavBar from '~layouts/NavBar';
-import { useState } from 'react';
-import { Tab } from '@headlessui/react';
-import { cn, toCapitalized } from '~utils/utils';
-import { getCurrentsUsers } from '~hooks/query-hooks/useCurrentUser';
-import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { HiOfficeBuilding } from "react-icons/hi";
+import { BiDesktop } from "react-icons/bi";
+import type { NextPage } from "next";
+import NavBar from "~layouts/NavBar";
+import { useState } from "react";
+import { Tab } from "@headlessui/react";
+import { cn, toCapitalized } from "~utils/utils";
+import { getCurrentsUsers } from "~hooks/query-hooks/useCurrentUser";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
 
 const Home: NextPage = () => {
-	const [viewMode, setViewMode] = useState<'mobilier' | 'immobilier'>('mobilier');
-
-	return (
-		<>
-			<NavBar
-				title={"Dashboard"}
-				navRight={() => (
-					<div className="flex items-center gap-4">
-						
-					</div>
-				)}
-			/>
-		</>
-	);
+  return (
+    <>
+      <NavBar
+        title={"Dashboard"}
+        navRight={() => <div className="flex items-center gap-4"></div>}
+      />
+    </>
+  );
 };
 
 export default Home;
