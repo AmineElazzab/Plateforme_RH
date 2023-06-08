3cdd4613398a1e3940c92f5efefddd7a52eb349d
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-08 12:44:48 +0100

    Update api_loaders.ts

--- client/src/loaders/api_loaders.ts
+++ client/src/loaders/api_loaders.ts
@@ -1,8 +1,8 @@
 import HttpClient, { KeyValue } from '../stores/http_store';
-import { IProject } from '../interfaces/project';
+import { IProject } from '~interfaces/project';
 
 export async function get_project(id: number): Promise<IProject> {
-  const url = `${HttpClient.base_url}project/${id}/`;
+  const url = `${HttpClient.base_url}/project/${id}/`;
   return HttpClient.get(url).then((response) => {
     if (response && response.status === 200) {
       const project: IProject = response.data;
@@ -21,7 +21,7 @@ export async function update_project() {}
 export async function delete_project() {}
 
 export async function get_users() {
-  const url = `${HttpClient.base_url}users`;
+  const url = `${HttpClient.base_url}/users`;
   return HttpClient.get(url).then((response) => {
     if (response && response.status === 200) {
       const users = response.data;
